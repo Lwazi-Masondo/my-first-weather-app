0434e2c10e67e871c1b6cf764cc1bc88d7a5724d
Nolwazi Masondo <masondop96@gmail.com>
2025-07-16 20:28:37 +0200

    Added more weather data for the current weather

--- weather-app/index.js
+++ weather-app/index.js
@@ -1,11 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
   const searchInput = document.getElementById("search-input");
   const city = document.getElementById("city");
+  let condition = document.querySelector(".condition");
+  let humidity = document.querySelector(".humidity");
+  let wind = document.querySelector(".wind");
+  let currentWeatherIcon = document.querySelector(".current-weather-icon");
+  let timeElement = document.querySelector(".time-element");
 
   function updateWeather(response) {
     let temperatureData = document.querySelector(".temperature-data");
+    let date = new Date(response.data.time * 1000);
     temperatureData.innerHTML = Math.round(response.data.temperature.current);
     city.innerHTML = response.data.city;
+    condition.innerHTML = response.data.condition.description;
+    humidity.innerHTML = response.data.temperature.humidity;
+    wind.innerHTML = response.data.wind.speed;
+    currentWeatherIcon.innerHTML = response.data.condition.icon;
+    timeElement.innerHTML = formateDate(date);
+    console.log(response.data);
+  }
+
+  function formateDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
   }
 
   function searchCity(city) {
